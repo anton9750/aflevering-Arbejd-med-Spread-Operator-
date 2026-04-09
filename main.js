b0612fceb6d0ba4485c3cf1b2958df40133e2d4e
Anton Olander Revsbæk Jensen <anto0784@elev.techcollege.dk>
2026-04-09 11:08:07 +0200

    i made the changes

---
+++ main.js
@@ -0,0 +1,72 @@
+
+const numbers = [1, 2, 3];  
+const numbas = [1, 2, 3];
+
+const moreNumbers = [...numbers, 4, 5];
+const morenumbas = {...numbas, 4: 4, 5: 5};
+
+console.log("Opgave 1:", moreNumbers);
+console.log("opgave 2:", morenumbas);
+
+const original = ["Anna", "Mads", "Lea"];
+const originale = ("Thomas", "Steffan", "Anakue")
+
+const copy = [...original];
+copy.push("Jonas");
+
+const copa = {...originale};
+copa.push("Nappa");
+
+console.log("Opgave 2 - original:", original);
+console.log("Opgave 2 - copy:", copy);
+
+
+
+
+
+const student = { name: "Anna", age: 25 };
+
+const updatedStudent = {
+  ...student,
+  age: 26
+};
+
+console.log("Opgave 3 - student:", student);
+console.log("Opgave 3 - updatedStudent:", updatedStudent);
+
+
+
+const a = { x: 1 };
+const b = { y: 2 };
+
+const all = {
+  ...a,
+  ...b
+};
+
+console.log("Opgave 4:", all);
+
+
+
+
+const average = (...numbers) => {
+  const sum = numbers.reduce((total, num) => total + num, 0);
+  return sum / numbers.length;
+};
+
+console.log("Opgave 5:", average(2, 4, 6));      
+console.log("Opgave 5:", average(1, 2, 3, 4, 5)); 
+
+
+
+
+
+const addItem = (list, item) => {
+  return [...list, item];
+};
+
+const list = [1, 2, 3];
+const newList = addItem(list, 4);
+
+console.log("Opgave 6 - original:", list);
+console.log("Opgave 6 - ny liste:", newList);
